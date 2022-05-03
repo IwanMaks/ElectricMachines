b1671e4a321d256cb9b1926965e8b15c3f54c49f
Iwan <iwan.maksackov@yandex.ru>
2022-05-03 14:21:34 +0300

    some refactor and add README.MD

--- src/App.js
+++ src/App.js
@@ -3,17 +3,17 @@ import './style.css'
 import './App.css'
 import { Task1_2 } from "./components/Task1_2/Task1_2";
 import { StartPage } from "./components/StartPage/StartPage";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { Routes, Route, HashRouter } from "react-router-dom";
 
 const App = () => {
     return (
-        <BrowserRouter>
+        <HashRouter>
             <Routes>
-                <Route path="/" element={<StartPage />} />
+                <Route path="" element={<StartPage />} />
                 <Route path="1" element={<Task1_2 type='task1' />} />
                 <Route path="2" element={<Task1_2 type='task2' />} />
             </Routes>
-        </BrowserRouter>
+        </HashRouter>
     )
 }
 
--- src/components/DataOutput/DataOutput.js
+++ src/components/DataOutput/DataOutput.js
@@ -9,21 +9,16 @@ export const DataOutput = ({arrowTop, s1Open, s2Open}) => {
         <div className="data-output">
             <div className="data-elem">
                 <div className="name-elem">P1=</div>
-                {/* <div className="value-elem">{(!s1Open && s2Open) ? 
-                            (Rhh * ((U1 / Zhh) * (U1 / Zhh))).toFixed(2) : 
-                            (!s1Open && !s2Open) ? (Rkz * ((U1 / Zkz) * (U1 / Zkz))).toFixed(2) :
-                            "0.0"
-                            }</div> */}
                 <div className="value-elem">{(!s1Open && s2Open) ? 
                             (Rhh * ((U1 / Zhh) * (U1 / Zhh))).toFixed(2) : 
                             (!s1Open && !s2Open) ? (Rkz * ((U1 / Zkz) * (U1 / Zkz))).toFixed(2) :
-                            "0.0"
+                            "0.00"
                             }</div>
                 <div className="name-elem">[Вт]</div>
             </div>
             <div className="data-elem">
                 <div className="name-elem">U1=</div>
-                <div className="value-elem">{!s1Open ? U1 : "0.0"}</div>
+                <div className="value-elem">{!s1Open ? U1.toFixed(2) : "0.00"}</div>
                 <div className="name-elem">[В]</div>
             </div>
             <div className="data-elem">
@@ -31,18 +26,18 @@ export const DataOutput = ({arrowTop, s1Open, s2Open}) => {
                 <div className="value-elem">{(!s1Open && s2Open) ? 
                             (U1 / Zhh).toFixed(3) : 
                             (!s1Open && !s2Open) ? (U1 / Zkz).toFixed(3) : 
-                            "0.0"
+                            "0.000"
                             }</div>
                 <div className="name-elem">[A]</div>
             </div>
             <div className="data-elem">
                 <div className="name-elem">U2=</div>
-                <div className="value-elem">{(!s1Open && s2Open) ? (U1 * k).toFixed(2) : "0.0"}</div>
+                <div className="value-elem">{(!s1Open && s2Open) ? (U1 * k).toFixed(2) : "0.00"}</div>
                 <div className="name-elem">[В]</div>
             </div>
             <div className="data-elem">
                 <div className="name-elem">I2=</div>
-                <div className="value-elem">{((!s1Open && s2Open) || (s1Open && s2Open) || (s1Open && !s2Open)) ? "0.0" : ((U1 / Zkz) / (k + 0.02)).toFixed(3)}</div>
+                <div className="value-elem">{((!s1Open && s2Open) || (s1Open && s2Open) || (s1Open && !s2Open)) ? "0.000" : ((U1 / Zkz) / (k + 0.02)).toFixed(3)}</div>
                 <div className="name-elem">[A]</div>
             </div>
         </div> 
